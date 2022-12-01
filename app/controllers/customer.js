6f4f57264b89901a1c944d9cb55bac6c0d64d232
Erik Salto Segarra <61243383+esalto98@users.noreply.github.com>
2022-12-01 01:19:49 -0500

    started customer signup endpt

---
+++ app/controllers/customer.js
@@ -0,0 +1,36 @@
+require("dotenv").config(); // load .env variables
+const { Router } = require("express"); // import router from express
+const Customer = require("../models/customer.model"); // import user model
+const bcrypt = require("bcryptjs"); // import bcrypt to hash passwords
+const jwt = require("jsonwebtoken"); // import jwt to sign tokens
+const customerSignupRouter = Router();
+
+//DESTRUCTURE ENV VARIABLES WITH DEFAULTS
+const { SECRET = "secret" } = process.env;
+
+// middleware that is specific to this router
+customerSignupRouter.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+customerSignupRouter.get("/", async (req, res) => { 
+  res.send("inside customer endpoint");
+
+});
+
+customerSignupRouter.post("/signup", async (req, res) => {
+  try {
+    // hash the password
+    // deepcode ignore HardcodedSecret: <please specify a reason of ignoring this>, deepcode ignore HardcodedSecret: <please specify a reason of ignoring this>
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    // create a new user
+    const customer = await Customer.create(req.body);
+    // send new user as response
+    res.json(customer);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+module.exports = customerSignupRouter;
--- app/startup/startup.js
+++ app/startup/startup.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // load .env variables
 const express = require("express"); // import express
 const morgan = require("morgan"); //import morgan
 const cors = require("cors");
-var signupRouter = require("../routes/signup");
+const customerSignupRouter = require("../controllers/customer");
 
 module.exports = function (app) {
   // GLOBAL MIDDLEWARE
@@ -10,7 +10,7 @@ module.exports = function (app) {
   app.use(morgan("tiny")); // log the request for debugging
   app.use(express.json()); // parse json bodies
 
-  app.use("/signup", signupRouter);
+  app.use("/customer", customerSignupRouter);
   //   app.use("/router1", router1Router);
   //   app.use("/router2", router2Router);
 };
