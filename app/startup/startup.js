7a6a484abeb36a8e345b50517061d586063f1de2
Erik Salto Segarra <61243383+esalto98@users.noreply.github.com>
2022-12-01 00:12:34 -0500

    connected db

--- app/startup/startup.js
+++ app/startup/startup.js
@@ -1,11 +1,16 @@
-const express = require("express");
+require("dotenv").config(); // load .env variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const cors = require("cors");
 var signupRouter = require("../routes/signup");
 
-
 module.exports = function (app) {
-  app.use(express.json());
+  // GLOBAL MIDDLEWARE
+  app.use(cors()); // add cors headers
+  app.use(morgan("tiny")); // log the request for debugging
+  app.use(express.json()); // parse json bodies
 
   app.use("/signup", signupRouter);
-//   app.use("/router1", router1Router);
-//   app.use("/router2", router2Router);
+  //   app.use("/router1", router1Router);
+  //   app.use("/router2", router2Router);
 };
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { log } = require("mercedlogger"); // import mercedlogger's log function
 const app = express();
 
 require("./app/startup/startup")(app);
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+//DESTRUCTURE ENV VARIABLES WITH DEFAULT VALUES
+const {PORT = 3000} = process.env
+app.get("/", (req, res) => {
+    res.send("welcome to water testing");
+})
+app.listen(PORT, () => log.green("SERVER STATUS", `Listening on port ${PORT}`));
